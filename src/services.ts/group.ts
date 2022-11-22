050100ad54d6b24e564276d4aa89be7935916ada
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-22 12:02:58 +0100

    Add try/catch

--- src/services.ts/group.ts
+++ src/services.ts/group.ts
@@ -10,34 +10,42 @@ export const addToGroup = async ({
   device: IDevice;
   groupId: string;
 }) => {
-  const deviceRes = await DeviceModel.create(device);
+  try {
+    const deviceRes = await DeviceModel.create(device);
 
-  const isGroup = await GroupModel.findById(groupId);
+    const isGroup = await GroupModel.findById(groupId);
 
-  if (!isGroup) {
-    const groupRes = await GroupModel.create({ devices: deviceRes._id });
-    return { obj: groupRes, status: 201 };
-  } else {
-    // findOneAndUpadte doesn't consider required: true  in the model ;'/
-    const groupRes = await GroupModel.findOneAndUpdate(
-      {
-        _id: groupId,
-      },
-      { $push: { devices: deviceRes._id } },
-      { upsert: true, new: true }
-    );
-    return { obj: groupRes, status: 204 };
+    if (!isGroup) {
+      const groupRes = await GroupModel.create({ devices: deviceRes._id });
+      return { obj: groupRes, status: 201 };
+    } else {
+      // findOneAndUpadte doesn't consider required: true  in the model ;'/
+      const groupRes = await GroupModel.findOneAndUpdate(
+        {
+          _id: groupId,
+        },
+        { $push: { devices: deviceRes._id } },
+        { upsert: true, new: true }
+      );
+      return { obj: groupRes, status: 204 };
+    }
+  } catch (error) {
+    return { obj: { mesage: " Error during DB operations" }, status: 500 };
   }
 };
 
 export const get = async (groupId: string[]) => {
-  const groups = await GroupModel.findOne({ _id: groupId });
-  if (groups) {
-    const devices = await DeviceModel.find({ $in: groups.devices });
-    const files = devices?.map((d: IDeviceModel) => d.files)?.flat();
-    return Array.from(new Set(files));
+  try {
+    const groups = await GroupModel.findOne({ _id: groupId });
+    if (groups) {
+      const devices = await DeviceModel.find({ $in: groups.devices });
+      const files = devices?.map((d: IDeviceModel) => d.files)?.flat();
+      return Array.from(new Set(files));
+    }
+    return { obj: { message: "ID not found" }, status: 400 };
+  } catch (error) {
+    return { obj: { mesage: " Error during DB operations" }, status: 500 };
   }
-  return { obj: { message: "ID not found" }, status: 400 };
 };
 
 export const remove = async ({
@@ -47,22 +55,26 @@ export const remove = async ({
   deviceId: string;
   groupId: string;
 }) => {
-  const isGroup = await GroupModel.findById(groupId);
-  if (!isGroup) {
-    return { obj: { message: "Group wasn't found" }, status: 202 };
-  } else {
-    // findOneAndUpadte doesn't consider required: true  in the model ;'/
-    const groupRes = await GroupModel.findOneAndUpdate(
-      {
-        _id: groupId,
-      },
-      { $pull: { devices: deviceId } },
-      { upsert: true, new: true }
-    );
-    if (groupRes.devices.length === 0) {
-      await GroupModel.deleteOne({ _id: groupId });
+  try {
+    const isGroup = await GroupModel.findById(groupId);
+    if (!isGroup) {
+      return { obj: { message: "Group wasn't found" }, status: 202 };
+    } else {
+      // findOneAndUpadte doesn't consider required: true  in the model ;'/
+      const groupRes = await GroupModel.findOneAndUpdate(
+        {
+          _id: groupId,
+        },
+        { $pull: { devices: deviceId } },
+        { upsert: true, new: true }
+      );
+      if (groupRes.devices.length === 0) {
+        await GroupModel.deleteOne({ _id: groupId });
+      }
+      return { obj: groupRes, status: 201 };
     }
-    return { obj: groupRes, status: 201 };
+  } catch (error) {
+    return { obj: { mesage: " Error during DB operations" }, status: 500 };
   }
 };
 
